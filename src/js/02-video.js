da8eac5db145b15e5c8806b56b3aa1ce39861e35
KsenyaKoss <roxenn34@gmail.com>
2023-03-20 23:01:47 +0100

    task-1-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Vimeo from "@vimeo/player";
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+    
+
+function onPlay(data){
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data))
+}
+player.on('timeupdate', throttle(onPlay, 1000));
+    // data is an object containing properties specific to that event
+
+const savedTime = localStorage.getItem("videoplayer-current-time");
+
+const parsedTime = JSON.parse(savedTime);
+
+if(savedTime){
+    player.setCurrentTime(parsedTime.seconds) 
+};
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,11 @@
+const throttle = require('lodash.throttle');
 
+const form = document.querySelector('.feedback-form');
+form.addEventListener('submit', throttle(saveInput, 500));
+
+function saveInput(ev) {
+    console.log(ev.target.name);
+    console.log(ev.target.value);
+    localStorage.setItem('ev.target.name', ev.target.value)
+
+}
