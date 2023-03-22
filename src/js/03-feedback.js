5bb0a99fb60b642325931496265a3c4c3fbf359e
KsenyaKoss <roxenn34@gmail.com>
2023-03-22 06:55:39 +0100

    task-3-fixed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,17 +1,21 @@
 const throttle = require('lodash.throttle');
 
 const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('[name="email"]');
+const messageInput = form.querySelector('[name="message"]');
+
 form.addEventListener('input', throttle(saveInput, 500));
 
 if(localStorage.getItem('feedback-form-state')){
-        form.elements.email.value = JSON.parse(localStorage.getItem('feedback-form-state')).email;
-        form.elements.message.value = JSON.parse(localStorage.getItem('feedback-form-state')).message;
+    emailInput.value = JSON.parse(localStorage.getItem('feedback-form-state')).email;
+    messageInput.value = JSON.parse(localStorage.getItem('feedback-form-state')).message;
     }
 
 const feedbackForm = {}
 
-function saveInput(ev){
-feedbackForm[ev.target.name] =ev.target.value;
+function saveInput(){
+feedbackForm.email = emailInput.value;
+feedbackForm.message = messageInput.value;
 localStorage.setItem("feedback-form-state", JSON.stringify(feedbackForm));
 }
 
